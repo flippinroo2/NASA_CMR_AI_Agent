cee964b8022d76f320686eb3bb67746fe319d553
flippinroo2 <flippinroo2@gmx.com>
2025-08-09 16:24:57 -0400

    .

--- project/tests/llm/workflow/test_workflow_manager.py
+++ project/tests/llm/workflow/test_workflow_manager.py
@@ -3,7 +3,7 @@ import unittest
 from typing import Any
 
 import pytest
-from langgraph.graph import CompiledStateGraph
+from langgraph.graph.state import CompiledStateGraph
 
 from src.ENUMS import LLM_PROVIDER
 from src.llm.agents.cmr_api_agent import CMRApiAgent
@@ -13,17 +13,20 @@ from src.llm.workflow.workflow_manager import WorkflowManager
 
 
 class TestCMRApiAgent(unittest.TestCase):
-    compiled_workflow_manager: CompiledStateGraph
+    compiled_workflow_manager: CompiledStateGraph | None = None
     test_query: str = "Why do you think 2024 had such powerful storms towards the end of the year?"  # TODO: Load the json file with test cases instead of using this hard coded string here.
-    test_state: AgentState
-    workflow_manager: WorkflowManager
+    test_state: AgentState | None = None
+    workflow_manager: WorkflowManager | None = None
 
     def setUp(self) -> None:
         test_state = AgentState(query=self.test_query)
         workflow_manager = WorkflowManager(LLMProvider(LLM_PROVIDER.OLLAMA))
-        compiled_workflow_manager = workflow_manager.workflow.compile()
+        compiled_workflow_manager: CompiledStateGraph = (
+            workflow_manager.workflow.compile()
+        )
+        print("DEBUG")
 
-    @pytest.mark.skip
     def test_01(self) -> None:
         test = self.compiled_workflow_manager
-        print("DEBUG")
+        if test is not None:
+            print("DEBUG")
